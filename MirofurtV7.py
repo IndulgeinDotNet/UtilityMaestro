88c5ac0d1290f97d1f0392e93f7c197dd83d063b
IndulgeinDotNet <125331271+IndulgeinDotNet@users.noreply.github.com>
2023-09-21 14:23:21 -0700

    Update MirofurtV7.py
    Added A Few

--- MirofurtV7.py
+++ MirofurtV7.py
@@ -5,8 +5,7 @@ import requests
 import scapy.all
 from scapy.sendrecv import sniff
 import urllib.parse
-
-from urllib3.util import parse_url
+import re
 
 # Constants
 CORRECT_ACCESS_CODE = "1234"
@@ -74,11 +73,42 @@ sql_injection_payloads = [
     },
 ]
 
+# Common vulnerability checks
+COMMON_VULNERABILITIES = [
+    {
+        "name": "Remote Code Execution",
+        "payload": "<?php system($_GET['cmd']); ?>",
+        "check": "Remote Code Execution Detected",
+    },
+    {
+        "name": "File Inclusion",
+        "payload": "../../../../etc/passwd",
+        "check": "File Inclusion Detected",
+    },
+    {
+        "name": "XSS (Cross-Site Scripting)",
+        "payload": '<script>alert("XSS Vulnerability Detected");</script>',
+        "check": "XSS Vulnerability Detected",
+    },
+    {
+        "name": "SQL Injection",
+        "payload": "1' OR '1'='1",
+        "check": "SQL Vulnerability Detected",
+    },
+    {
+        "name": "Command Injection",
+        "payload": "; ls -la",
+        "check": "Command Vulnerability Detected",
+    },
+    # Add more vulnerability checks as needed
+]
+
 # Function to create a tool frame
 def create_tool_frame(tool_name, notebook):
     tool_frame = ttk.Frame(notebook)
     notebook.add(tool_frame, text=tool_name)
     return tool_frame
+
 
 # Function to update the access button state
 def update_access_button_state():
@@ -87,10 +117,12 @@ def update_access_button_state():
     else:
         access_button.configure(state=tk.DISABLED)
 
+
 # Function to create a label
 def create_label(parent, text, font=None, pady=0):
     label = ttk.Label(parent, text=text, font=font)
     label.pack(pady=pady)
+
 
 # Function to create a scrolled text widget
 def create_scrolled_text(parent, wrap, width, height, padx=0, pady=0):
@@ -98,17 +130,20 @@ def create_scrolled_text(parent, wrap, width, height, padx=0, pady=0):
     text_widget.pack(padx=padx, pady=pady)
     return text_widget
 
+
 # Function to create an entry widget
 def create_entry(parent, width):
     entry = ttk.Entry(parent, width=width)
     entry.pack()
     return entry
 
+
 # Function to create a button
 def create_button(parent, text, command):
     button = ttk.Button(parent, text=text, command=command)
     button.pack()
     return button
+
 
 # Function to perform port scanning
 def port_scan(host, port_range, output_text):
@@ -127,6 +162,7 @@ def port_scan(host, port_range, output_text):
     except Exception as e:
         output_text.insert(tk.END, f"An error occurred: {str(e)}\n")
 
+
 # Function to perform vulnerability scanning
 def vulnerability_scan(target_url, output_text):
     try:
@@ -135,21 +171,63 @@ def vulnerability_scan(target_url, output_text):
 
         if response.status_code == 200:
             output_text.insert(tk.END, "Target web application is online.\n")
+            vulnerabilities_found = []
+            for vulnerability in COMMON_VULNERABILITIES:
+                payload = vulnerability["payload"]
+                check = vulnerability["check"]
+                payload_response = requests.get(target_url + payload)
+                if re.search(check, payload_response.text, re.IGNORECASE):
+                    vulnerabilities_found.append(vulnerability["name"])
+
+            if vulnerabilities_found:
+                output_text.insert(tk.END, "Vulnerabilities detected:\n")
+                for vuln in vulnerabilities_found:
+                    output_text.insert(tk.END, f"- {vuln}\n")
+            else:
+                output_text.insert(tk.END, "No vulnerabilities detected.\n")
         else:
             output_text.insert(tk.END, f"Failed to connect to the target URL. Status code: {response.status_code}\n")
 
     except Exception as e:
         output_text.insert(tk.END, f"An error occurred: {str(e)}\n")
-
 # Function to perform password cracking
 def password_cracker(target_password, dictionary, output_text):
-    # Implement password cracking functionality here
-    pass
+    # Iterate through the dictionary of passwords
+    for password in dictionary:
+        # Check if the current password matches the target password
+        if password == target_password:
+            # Password found, write it to the output file
+            with open(output_text, 'w') as output_file:
+                output_file.write(f"Password found: {password}\n")
+            return
 
-# Function to start password cracking
+    # If the loop completes without finding the password, write a failure message
+    with open(output_text, 'w') as output_file:
+        output_file.write("Password not found\n")
+
+
 def start_password_cracking(target_password, dictionary_file, output_text):
-    # Implement password cracking startup functionality here
-    pass
+    try:
+        # Read the dictionary file and split it into a list of passwords
+        with open(dictionary_file, 'r') as dictionary_file:
+            dictionary = [line.strip() for line in dictionary_file.readlines()]
+
+        # Call the password cracker function
+        password_cracker(target_password, dictionary, output_text)
+    except FileNotFoundError:
+        print("Dictionary file not found.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+
+# Example usage:
+if __name__ == "__main__":
+    target_password = "target_password"
+    dictionary_file = "passwords.txt"  # Replace with your dictionary file path
+    output_text = "output.txt"  # Replace with your output file path
+
+    start_password_cracking(target_password, dictionary_file, output_text)
+
 
 # Function to execute SQL injection testing
 def execute_sql_injection(target_url, sql_payload, output_text):
@@ -170,6 +248,7 @@ def execute_sql_injection(target_url, sql_payload, output_text):
     except Exception as e:
         output_text.insert(tk.END, f"An error occurred: {str(e)}\n")
 
+
 # Function to create the Port Scanner tool
 def create_port_scanner_tool(tool_frame):
     create_label(tool_frame, "Port Scanner", font=("Helvetica", 16), pady=10)
@@ -177,16 +256,17 @@ def create_port_scanner_tool(tool_frame):
     host_entry = create_entry(tool_frame, 40)
     port_range_label = create_label(tool_frame, "Port Range (e.g., 80-100):")
     port_range_entry = create_entry(tool_frame, 40)
-    output_text = create_scrolled_text(tool_frame, tk.WORD, 40, 10, padx=10, pady=10)
+    output_text = create_scrolled_text(tool_frame, tk.WORD, 80, 18, padx=10, pady=10)
 
     create_button(tool_frame, "Start Scan", lambda: port_scan(host_entry.get(), port_range_entry.get(), output_text))
+
 
 # Function to create the Vulnerability Scanner tool
 def create_vulnerability_scanner_tool(tool_frame):
     create_label(tool_frame, "Vulnerability Scanner", font=("Helvetica", 16), pady=10)
     target_label = create_label(tool_frame, "Target URL:")
     target_entry = create_entry(tool_frame, 40)
-    output_text = create_scrolled_text(tool_frame, tk.WORD, 40, 10, padx=10, pady=10)
+    output_text = create_scrolled_text(tool_frame, tk.WORD, 80, 18, padx=10, pady=10)
 
     create_button(tool_frame, "Start Scan", lambda: vulnerability_scan(target_entry.get(), output_text))
 
@@ -198,7 +278,7 @@ def create_password_cracker_tool(tool_frame):
     dictionary_label = create_label(tool_frame, "Dictionary File:")
     dictionary_entry = create_entry(tool_frame, 50)
     result_label = create_label(tool_frame, "Result:")
-    result_text = create_scrolled_text(tool_frame, tk.WORD, 60, 10, padx=10, pady=10)
+    result_text = create_scrolled_text(tool_frame, tk.WORD, 80, 18, padx=10, pady=10)
 
     create_button(tool_frame, "Start Cracking",
                   lambda: start_password_cracking(password_entry.get(), dictionary_entry.get(), result_text))
@@ -206,6 +286,18 @@ def create_password_cracker_tool(tool_frame):
 # Function to create the Network Sniffer tool
 def create_network_sniffer_tool(tool_frame):
     create_label(tool_frame, "Network Sniffer", font=("Helvetica", 16), pady=10)
+
+    # Create a label to explain the color code
+    color_code_label = ttk.Label(tool_frame, text="Color Code:")
+    color_code_label.pack(anchor="w", padx=10)
+
+    color_code_explanation = ttk.Label(
+        tool_frame,
+        text="- Red: Error messages\n- Green: Success messages\n- Blue: Packets",
+        foreground="Orange",
+        wraplength=400,
+    )
+    color_code_explanation.pack(anchor="w", padx=30)
 
     output_tree = ttk.Treeview(tool_frame, columns=("Packet Info",), show="headings")
     output_tree.heading("Packet Info", text="Packet Info")
@@ -218,7 +310,13 @@ def create_network_sniffer_tool(tool_frame):
         if not stop_sniffing_flag:
             packet_info = packet.summary()
             packet_list.append(packet_info)
-            output_tree.insert("", "end", values=(packet_info,))
+            # Determine the message type based on packet_info (you can modify this logic)
+            if "error" in packet_info.lower():
+                output_tree.insert("", "end", values=(packet_info,), tags=("error",))
+            elif "success" in packet_info.lower():
+                output_tree.insert("", "end", values=(packet_info,), tags=("success",))
+            else:
+                output_tree.insert("", "end", values=(packet_info,), tags=("info",))
             output_tree.see(output_tree.get_children()[-1])  # Scroll to the latest packet
 
     def start_sniffing():
@@ -263,15 +361,23 @@ def create_network_sniffer_tool(tool_frame):
     save_checkbox = ttk.Checkbutton(tool_frame, text="Save to File", variable=save_checkbox_var)
     save_checkbox.pack()
 
+    # Apply different colors to text based on tags
+    output_tree.tag_configure("error", foreground="red")
+    output_tree.tag_configure("success", foreground="green")
+    output_tree.tag_configure("info", foreground="blue")
+
+
 # Function to authenticate access
 def authenticate_access():
     entered_access_code = access_code_entry.get()
     if entered_access_code == CORRECT_ACCESS_CODE and acknowledgment_accepted.get():
         open_tools()
     elif not acknowledgment_accepted.get():
-        messagebox.showerror("Acknowledgment Required", "You must accept the acknowledgment before accessing the tools.")
+        messagebox.showerror("Acknowledgment Required",
+                             "You must accept the acknowledgment before accessing the tools.")
     else:
         messagebox.showerror("Access Code Error", "Incorrect access code")
+
 
 # Function to open the toolset
 def open_tools():
@@ -293,10 +399,12 @@ def open_tools():
         elif tool_name == "SQL Injection Tool":
             create_sql_injection_tool(tool_frame)
 
+
 # Function to create an information label
 def create_info_label(tool_frame, info_text):
     info_label = ttk.Label(tool_frame, text=info_text, wraplength=380)
     info_label.pack(padx=10, pady=10)
+
 
 # Function to create a red label
 def create_red_label(parent, text, pady=0):
@@ -304,17 +412,26 @@ def create_red_label(parent, text, pady=0):
     label.pack(pady=pady)
     return label
 
+
 # Function to create the SQL Injection Tool
 def create_sql_injection_tool(tool_frame):
     create_label(tool_frame, "SQL Injection Tool", font=("Helvetica", 16), pady=10)
     target_label = create_label(tool_frame, "Target URL:")
     target_entry = create_entry(tool_frame, 50)
     payload_label = create_label(tool_frame, "SQL Payload:")
+    target_url_label = create_label(tool_frame, "Target URL:")
+    target_url_entry = create_entry(tool_frame, 50)
+
+    # Create a label and combo box for selecting payloads
+    payload_label = create_label(tool_frame, "Select Payload:")
+    payload_combo = ttk.Combobox(tool_frame, values=[payload["description"] for payload in sql_injection_payloads],
+                                 width=50)
 
     # Create a StringVar to store the selected payload description
     selected_payload_description = tk.StringVar()
 
-    payload_combo = ttk.Combobox(tool_frame, values=[payload["payload"] for payload in sql_injection_payloads], width=50)
+    payload_combo = ttk.Combobox(tool_frame, values=[payload["payload"] for payload in sql_injection_payloads],
+                                 width=50)
     payload_combo.pack()
 
     # Create and update the payload description label
@@ -322,9 +439,49 @@ def create_sql_injection_tool(tool_frame):
     description_label.config(textvariable=selected_payload_description)
 
     result_label = create_label(tool_frame, "Result:")
-    result_text = create_scrolled_text(tool_frame, tk.WORD, 60, 10, padx=10, pady=10)
+    result_text = create_scrolled_text(tool_frame, tk.WORD, 80,10, padx=10, pady=10)
+    result_text.tag_configure("error", foreground="red")
+    result_text.tag_configure("success", foreground="green")
+    result_text.tag_configure("info", foreground="blue")
+
 
     # Function to update the selected payload's description
+    def execute_sql_injection_with_params():
+        try:
+            # Get the selected payload description and find the corresponding payload
+            selected_payload_description = payload_combo.get()
+            payload_index = next(
+                (i for i, payload in enumerate(sql_injection_payloads) if
+                 payload["description"] == selected_payload_description), -1)
+
+            if payload_index != -1:
+                # Get the selected payload from the list
+                sql_payload = sql_injection_payloads[payload_index]["payload"]
+                # Get the target URL from the entry field
+                target_url = target_url_entry.get()
+
+                # Construct a parameterized query
+                payload = {"param": sql_payload}
+                response = requests.get(target_url, params=payload)
+
+                if "error" in response.text.lower():
+                    result_text.insert(tk.END, "SQL Injection Detected!\n", "error")
+                else:
+                    result_text.insert(tk.END, "No SQL Injection Detected.\n", "success")
+
+                # Present the response content with improved formatting
+                formatted_response = response.text.replace('\n', '\n    ')
+                result_text.insert(tk.END, "Response Content:\n", "info")
+                result_text.insert(tk.END, f"\n    {formatted_response}\n")
+            else:
+                result_text.insert(tk.END, "Invalid payload selected.\n", "error")
+
+        except Exception as e:
+            result_text.insert(tk.END, f"An error occurred: {str(e)}\n", "error")
+
+    # Create a button to execute SQL injection with parameterized queries
+    execute_button = create_button(tool_frame, "Execute SQL Injection (Parameterized)",
+    execute_sql_injection_with_params)
     def update_payload_description(event):
         selected_payload = payload_combo.get()
         payload_index = next(
@@ -337,7 +494,9 @@ def create_sql_injection_tool(tool_frame):
     # Bind the update_payload_description function to the payload combo box
     payload_combo.bind("<<ComboboxSelected>>", update_payload_description)
 
-    create_button(tool_frame, "Execute SQL Injection", lambda: execute_sql_injection(target_entry.get(), payload_combo.get(), result_text))
+    create_button(tool_frame, "Execute SQL Injection",
+                  lambda: execute_sql_injection(target_entry.get(), payload_combo.get(), result_text))
+
 
 # Main application setup
 app = tk.Tk()
@@ -373,10 +532,11 @@ access_code_entry.pack()
 access_button = ttk.Button(access_frame, text="Access Tools", command=authenticate_access)
 access_button.pack()
 
-info_label = ttk.Label(access_frame, text="This is a versatile utility suite. Please enter the access code to access the tools.")
+info_label = ttk.Label(access_frame,
+                       text="This is a versatile utility suite. Please enter the access code to access the tools.")
 info_label.pack(pady=10)
 
-info_label = ttk.Label(access_frame, text="Created By IndulgeIn. Version 0.778- Mirofurt", font=("Sans-serif", 7))
+info_label = ttk.Label(access_frame, text="Created By IndulgeIn. Version 0.8- Mirofurt", font=("Sans-serif", 7))
 info_label.pack()
 
 acknowledgment_accepted = tk.BooleanVar(value=False)
